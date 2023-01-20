57856ff4ece7f63af31de2f9b179bbe32374993f
leaco <leaco.li@learnable.ai>
2023-01-20 14:45:17 +0800

    upgrade

--- src/main/kotlin/pro/leaco/console/qrcode/ConsoleQrcode.kt
+++ src/main/kotlin/pro/leaco/console/qrcode/ConsoleQrcode.kt
@@ -26,6 +26,11 @@ object ConsoleQrcode {
         25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, -1, -1, -1, -1, -1
     )
 
+    /**
+     * print qrcode in console
+     *
+     * @param content the qrcode's content
+     */
     fun print(content: String) {
         val hints: MutableMap<EncodeHintType, Any?> = EnumMap(EncodeHintType::class.java)
         hints[EncodeHintType.CHARACTER_SET] = "UTF-8"
